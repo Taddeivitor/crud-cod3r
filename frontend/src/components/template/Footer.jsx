d7918be705eea99204d7626d10dee88f3388e39a
Taddeivitor <taddeivitor@hotmail.com>
2020-07-09 18:33:15 -0300

    Projeto crud com react

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Footer.css'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> Vitor <span className="text-danger">Taddei</span></strong>
+        </span>
+    </footer>
